0ce205a535bca6420afe42f2c7ba83906da4c1fa
HarrisonMatthews <S01159654@acad.tri-c.edu>
2020-04-06 16:48:25 -0400

    Music Store Application

---
+++ MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using MVCMusicStoreApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        public ActionResult Index(int id)
+        {
+            var albums = db.Albums.Where(a => a.GenreId == id)
+                .ToList();
+            return View(albums);
+        }
+        
+        public ActionResult Browse(string genre)
+        {
+            var genres = db.Genres.ToList();
+            return View(genres);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var album = db.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
+    
---
+++ MVCMusicStoreApplication/Models/MVCMusicStoreArtist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStoreApplication.Models
+{
+    public class MVCMusicStoreArtist : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MVCMusicStoreArtist() : base("name=MVCMusicStoreArtist")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MVCMusicStoreApplication.Models.Artist> Artists { get; set; }
+    }
+}
